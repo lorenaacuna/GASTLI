eddd55e140c1feed9c8df2072a505d5ae81ae8c6
lorenaacuna <66270562+lorenaacuna@users.noreply.github.com>
2024-07-05 15:38:26 +0200

    Update of test suite

---
+++ test_Coupling.py
@@ -0,0 +1,46 @@
+
+
+# Import coupling module
+import gastli.Coupling as cpl
+
+# Other Python modules
+import numpy as np
+
+# Create coupling class
+my_coupling = cpl.coupling(path_to_file="/Users/acuna/Desktop/gastli_input_data/",
+                           j_max=99,pow_law_formass=0.315)
+
+
+# Input for interior
+# 1 Mjup in Mearth units
+Mjup = 318.
+# M = 1 Mjup
+M_P = Mjup
+# Internal and equilibrium temperatures
+Tintpl = 107.
+Teqpl = 105.
+
+# Core mass fraction and envelope metal mass fraction
+CMF = 0.01
+
+
+
+# Call to coupled interior-atm. model (and time it)
+# Case 1, log(Fe/H) is known
+my_coupling.main(M_P, CMF, Teqpl, Tintpl, CO=0.55,log_FeH=0.)
+
+
+Zenv_expected = 0.0135335763819442
+Mtot_expected = 318.0390773794476
+T_surf_expected = 1328.9730303240333
+Rbulk_Rjup_expected = 0.9738243869527924
+Rtot_expected = 0.9799956272275188
+
+def test_answer():
+    assert my_coupling.myatmmodel.Zenv_pl == Zenv_expected
+    assert my_coupling.Mtot == Mtot_expected
+    assert my_coupling.T_surf == T_surf_expected
+    assert my_coupling.Rbulk_Rjup == Rbulk_Rjup_expected
+    assert my_coupling.Rtot == Rtot_expected
+
+
---
+++ test_GASTLI.py
@@ -0,0 +1,34 @@
+
+
+# Import interior module
+from gastli.GASTLI import int_planet
+
+# Other Python modules
+import numpy as np
+
+# Change path accordingly
+path_to_input = "/Users/acuna/Desktop/gastli_input_data/"
+myplanet_test = int_planet(path_to_file=path_to_input)
+
+n_lay_expected = int(3)
+Ttp_expected = np.array([273.16,251.165,256.164,273.31,355.,647.096])
+ilayer_expected = np.array([1,2,3])
+
+myplanet_test.setup_parameters()
+
+M_P = 318.
+x_core = 0.10
+T_surf = 1300.
+P_surf = 1e3*1e5
+Zenv = 0.04
+myplanet_test.calc_radius(M_P,x_core,1-x_core,T_surf,P_surf,Zenv)
+
+expected_radius = 10.188813052994131
+
+def test_answer():
+    assert myplanet_test.n_lay == n_lay_expected
+    assert len(myplanet_test.Ttp) == len(Ttp_expected)
+    assert all([a == b for a, b in zip(myplanet_test.Ttp, Ttp_expected)])
+    assert len(myplanet_test.ilayer) == len(ilayer_expected)
+    assert all([a == b for a, b in zip(myplanet_test.ilayer, ilayer_expected)])
+    assert myplanet_test.R_P == expected_radius
